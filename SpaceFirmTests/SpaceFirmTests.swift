718f27a2728c36c66a14a4ca150e86b51d9ecf18
chahelpaatur <chahelpaatur@gmail.comgit config --global user.email chahelpaatur@gmail.com>
2025-03-03 17:30:11 -0800

    Initial Commit

---
+++ SpaceFirmTests/SpaceFirmTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceFirmTests.swift
+//  SpaceFirmTests
+//
+//  Created by Chahel Paatur on 3/3/25.
+//
+
+import Testing
+@testable import SpaceFirm
+
+struct SpaceFirmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
